bd2b56287e3608a449bb0b839fe1a784a57c3278
savefile <kasshust@gmail.com>
2024-10-19 19:05:17 +0900

    機動性修正

--- Assets/Project/PRJ51/GameTestManager.cs
+++ Assets/Project/PRJ51/GameTestManager.cs
@@ -1,10 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pixeye.Unity;
 
 public class GameTestManager : MonoBehaviour
 {
     [SerializeField] PlatformCamera m_Camera;
+
+    [SerializeField, Foldout("BaseInfo")]
+    public PlatformCamera.CameraAreaInfo m_CameraAreaInfo;
+
+    [SerializeField, Foldout("Debug View")]
+    bool isDrawDebug = false;
+
+    private void Awake()
+    {
+        m_CameraAreaInfo.m_AreaCenter = transform.position;
+        SendCameraInfo();
+    }
 
     void Start()
     {
@@ -15,4 +28,22 @@ public class GameTestManager : MonoBehaviour
     {
         
     }
+
+    public void SendCameraInfo()
+    {
+        if (m_Camera != null) m_Camera.SetCameraArea(m_CameraAreaInfo);
+    }
+
+    public void ResetCameraInfo()
+    {
+        if (m_Camera != null) m_Camera.ResetCameraArea();
+    }
+
+#if UNITY_EDITOR
+
+    private void OnDrawGizmos()
+    {
+        if (isDrawDebug) Gizmos.DrawCube((Vector3)m_CameraAreaInfo.m_AreaOffset + transform.position, (Vector3)m_CameraAreaInfo.m_CameraMoveAreaSize + Vector3.forward);
+    }
+#endif
 }
